c470542595a2661bd2f0904f867c5e3dc2bdf833
bschwartz10 <bschwartz92@gmail.com>
2017-07-02 22:41:01 -0600

    completed unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,3 +1,14 @@
-function Unicorn() {}
+function Unicorn(name, color = 'white') {
+  this.name = name
+  this.color = color
+}
+
+Unicorn.prototype.isWhite = function() {
+  return (this.color === 'white') ? true : false
+}
+
+Unicorn.prototype.says = function(saying) {
+  return `**;* ${saying} *;**`
+}
 
 module.exports = Unicorn;
